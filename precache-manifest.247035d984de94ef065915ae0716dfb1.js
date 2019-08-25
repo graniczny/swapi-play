c9d8611dc80b19df4f270099d08ceafe4ee7363f
Rafal Graniczny <granicznyrafal@gmail.com>
2019-08-25 19:23:03 +0200

    Updates

---
+++ precache-manifest.247035d984de94ef065915ae0716dfb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0755fd7d9b7c495b4fef9f78ed5d945f",
+    "url": "/swapi-play/index.html"
+  },
+  {
+    "revision": "9aefb5bff58665fef27e",
+    "url": "/swapi-play/static/css/main.8f077e1c.chunk.css"
+  },
+  {
+    "revision": "dde8473d60b7d6d02d11",
+    "url": "/swapi-play/static/js/2.f0516b4d.chunk.js"
+  },
+  {
+    "revision": "9aefb5bff58665fef27e",
+    "url": "/swapi-play/static/js/main.2ef27f20.chunk.js"
+  },
+  {
+    "revision": "1f9a78f16aac1ced794c",
+    "url": "/swapi-play/static/js/runtime~main.4183534b.js"
+  },
+  {
+    "revision": "95cebd7ef397d17963a1b8efe0ba0e12",
+    "url": "/swapi-play/static/media/deathstar.95cebd7e.png"
+  },
+  {
+    "revision": "bd69c16a4da953343dba68318bcfb9ae",
+    "url": "/swapi-play/static/media/loading.bd69c16a.gif"
+  },
+  {
+    "revision": "a51b1fe9f66bbbea7d5781f989ceb46f",
+    "url": "/swapi-play/static/media/logo.a51b1fe9.webp"
+  }
+]);
